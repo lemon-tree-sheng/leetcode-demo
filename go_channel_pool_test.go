5ceaa52ce51fe118583fe68d9deb67011e386c5e
sheng <sheng_704@126.com>
2023-11-16 23:41:28 +0800

    1

---
+++ go_channel_pool_test.go
@@ -0,0 +1,46 @@
+package leetcode_demo
+
+import (
+	"sync"
+	"testing"
+)
+
+type Pool struct {
+	tasks chan func()
+}
+
+func initPool() Pool {
+	p := Pool{
+		tasks: make(chan func(), 10),
+	}
+
+	go func() {
+		for {
+			select {
+			case t, ok := <-p.tasks:
+				if !ok {
+					go t()
+				}
+			}
+		}
+	}()
+
+	return p
+}
+
+func (p Pool) addTask(f func()) {
+	p.tasks <- f
+}
+
+func TestPool(t *testing.T) {
+	pool := initPool()
+	for i := 0; i < 10; i++ {
+		pool.addTask(func() {
+			println("foo")
+		})
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	wg.Wait()
+}
